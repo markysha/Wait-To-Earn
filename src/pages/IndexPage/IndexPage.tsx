7fcf8979bae2b75528bcb955b9bce9d49ab8a998
Mark Korneychik <markkornwork@gmail.com>
2024-06-06 19:57:09 +0300

    fix

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -9,7 +9,7 @@ import './IndexPage.css';
 
 export const IndexPage: Component = () => {
   return (
-    <Page title="Home Page">
+    <Page>
       <p class="index-page__p">
         This page is a home page in this boilerplate. You can use the links below to visit other
         pages with their own functionality.
